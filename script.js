4e6eb379bec0ed901424879cc2d39995171550a5
Rohisha Ramauthat <rohisharamauthar@gmail.com>
2021-05-02 13:29:21 +0200

    commit 'master'

--- script.js
+++ script.js
@@ -59,9 +59,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
-  movements.forEach(function (mov, i) {
+  
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+ movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -204,6 +207,12 @@ btnClose.addEventListener('click', function (e) {
     containerApp.style.opacity = 0;
   }
   inputCloseUsername.value = inputClosePin.value = '';
+});
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 
 /////////////////////////////////////////////////
@@ -402,6 +411,7 @@ console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
 */
 
+/*
 const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
 console.log(arr.flat());
 
@@ -420,4 +430,35 @@ const overallBalance2 = accounts
   .flatMap(acc => acc.movements)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overallBalance2);
+*/
+
+// Strings
+const owners = ['Kai', 'Trinesan', 'Gray'];
+console.log(owners.sort());
+console.log(owners);
+
+
+// Numbers
+console.log(movements);
+
+// return < 0, A, B (Keep order)
+// return > 0 B, A (Switch order)
+
+// Ascending
+// movements.sort((a, b) => {
+  // if (a > b) return 1;
+  // if (a < b) return -1;
+// });
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+// Descending
+// movements.sort((a, b) => {
+  // if (a > b) return -1;
+  // if (a < b) return 1;
+// });
+movements.sort((a, b) => b - a);
+console.log(movements);
+
+
 
